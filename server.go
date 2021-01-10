d9ce085280a98a3428e3a160ea41f3c41675d64c
Robles AR <tinolabs@yandex.com>
2021-01-10 12:29:49 -0300

    cors

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/roblesok/gorm-fiber-crud/controller"
 )
@@ -17,10 +18,9 @@ func main() {
 	app := fiber.New()
 
 	app.Use(logger.New())
+	app.Use(cors.New())
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("This Works")
-	})
+	app.Group("/api", middleware)
 
 	app.Get("/books", bookController.GetAllBooks)
 	app.Get("/books/:id", bookController.GetBook)
